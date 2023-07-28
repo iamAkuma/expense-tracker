dd77d6f76cb06b063339620df1ee79a1ceb9cfb2
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-28 19:06:18 +0545

    Frontend mostly done

---
+++ src/components/TransactionList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TransactionList = () => {
+  return (
+    <>
+    <h3>History</h3>
+    <ul className="list">
+        <li className="minus">
+            Cash <span>-Rs.400</span> <button className='delete-btn'>X</button>
+        </li>
+    </ul>
+  </>
+  )
+}
+
+export default TransactionList
