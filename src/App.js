0a5023b022fa8aaf325399fd850ff29efcda8267
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-28 18:57:18 +0545

    doing frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import Header from './components/Header'
+import Balance from './components/Balance'
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello
+      <Header/>
+      <div className="container">
+        <Balance/>
+      </div>
     </div>
   );
 }
