2b3a4738cb115ecddb6c87a9e4fc08072eb65806
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-29 18:03:19 +0545

    context added in balance

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState' 
 
 const IncomeExpenses = () => {
+  const {transactions} = useContext(GlobalContext);
+
   return (
     <div className='inc-exp-container'>
         <div>
             <h4>Income</h4>
-            <p id="money-plus" className='money plus'>+0.00</p>
+            <p  className='money plus'>+0.00</p>
         </div>
         <div>
             <h4>Expense</h4>
-            <p id="money-minus" className='money minus'>-0.00</p>
+            <p  className='money minus'>-0.00</p>
         </div>
       
     </div>
